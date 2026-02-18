c2554e6dae84e802500b73c22ca15604dc21bb76
David Federman <dfederm@microsoft.com>
2026-02-17 23:17:06 -0800

    Add sort and filer option to Library page

--- src/JellyBox/Views/Library.xaml.cs
+++ src/JellyBox/Views/Library.xaml.cs
@@ -18,5 +18,13 @@ internal sealed partial class Library : Page
 
     protected override void OnNavigatedTo(NavigationEventArgs e) => ViewModel.HandleParameters((Parameters)e.Parameter);
 
+    private void SortListView_ItemClick(object sender, ItemClickEventArgs e)
+    {
+        if (e.ClickedItem is SortOption sortOption)
+        {
+            ViewModel.SelectSortOptionCommand.Execute(sortOption);
+        }
+    }
+
     internal sealed record Parameters(Guid CollectionItemId, BaseItemKind ItemKind, string Title);
 }
